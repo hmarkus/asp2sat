8736c791d979367f921c8da31f33db02f1b66dd4
macus <macus@MacBook-Air.local>
2020-12-04 20:10:10 +0100

    fixed several issues, prepared td guided reduction

--- bin/main.py
+++ bin/main.py
@@ -75,16 +75,24 @@ class Application(object):
     def _generatePrimalGraph(self):
         self._graph = hypergraph.Hypergraph()
         self._program = []
+        self._atomToVertex = {} # htd wants succinct numbering of vertices / no holes
+        self._vertexToAtom = {} # inverse mapping of _atomToVertex 
         for o in self.control.ground_program.objects:
             if isinstance(o, ClingoRule):
                 o.atoms = set(o.head)
                 o.atoms.update(tuple(map(abs, o.body)))
-                #atoms = list(o.head)
-                #atoms.extend(o.body)
-                self._graph.add_hyperedge(o.atoms)
-                self._program.append(o)
-                #edges = ((abs(a),abs(b)) for a in atoms for b in atoms if abs(a) > abs(b))
-                #self._graph.add_edges_from(edges)
+                if len(o.atoms) > 1: # trivial rules / facts do not need to be considered
+                    for a in o.atoms.difference(self._atomToVertex):	# add mapping for atom not yet mapped
+                        vertex = len(self._atomToVertex) + 1
+                        self._atomToVertex[a] = vertex
+                        self._vertexToAtom[vertex] = a
+                    #atoms = list(o.head)
+                    #print(self._atomToVertex)
+                    #atoms.extend(o.body)
+                    self._program.append(o)
+                    self._graph.add_hyperedge(tuple(map(lambda x: self._atomToVertex[x], o.atoms)))
+                    #edges = ((abs(a),abs(b)) for a in atoms for b in atoms if abs(a) > abs(b))
+                    #self._graph.add_edges_from(edges)
 
     def _decomposeGraph(self):
         # Run htd
@@ -98,7 +106,7 @@ class Application(object):
         logger.info("Running htd")
         self._graph.write_graph(p.stdin, dimacs=False, non_dimacs="tw")
         #with open('graph.txt', mode='wb') as file_out:
-        #    self._graph.write_gr(file_out)
+        #    self._graph.write_graph(file_out, dimacs=False, non_dimacs="tw")
         p.stdin.close()
         tdr = reader.TdReader.from_stream(p.stdout)
         p.wait()
@@ -116,25 +124,34 @@ class Application(object):
         # temporary copy of the program, will be empty after the first pass
         program = list(self._program)
         # first td pass: determine rules and prove_atoms
-        for t in self._td:
+        for t in self._td.nodes:
             prove_atoms[t] = set()
             rules[t] = []
+            # compute t.atoms
+            t.atoms = set(map(lambda x: self._vertexToAtom[x], t.vertices))
             # we require prove_atoms for t if it is contained in the bag and among prove_atoms of some child node
             for tp in t.children:
-                prove_atoms[t].update(prove_atoms[tp].intersection(t.vertices))
+                prove_atoms[t].update(prove_atoms[tp].intersection(t.atoms))
             # FIXME: python does not have c++-like enumerator to modify list during iteration, right?
             i = 0
             while i < len(program):
                 r = program[i]
-                if r.atoms in t.vertices:
+                #print(r, r.atoms, t.atoms)
+                if r.atoms.issubset(t.atoms):
+                    #print("subset")
                     prove_atoms[t].update(r.head)
                     rules[t].append(r)
                     program.remove(r)
                     i -= 1
                 i += 1
-
+        logger.info("program")
+        logger.info(rules)
+        logger.info("prove_atoms")
+        logger.info(prove_atoms)
+        logger.info("atoms to vertices")
+        logger.info(self._atomToVertex)
         # second td pass: use rules and prove_atoms to generate the reduction
-        for t in self._td:
+        for t in self._td.nodes:
             pass
 
     def main(self, clingo_control, files):
